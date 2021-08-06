3f4b2d81d62a7018b7b75160fb401ef06289a428
karol-yo2 <01160780@pw.edu.pl>
2021-08-06 18:33:35 +0200

    nie lubie githuba

--- Table.cs
+++ Table.cs
@@ -2,10 +2,6 @@
 using System.IO;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ConsoleApp1
 {
     class Table
